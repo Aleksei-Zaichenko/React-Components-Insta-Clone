92d2f497741071b8736368fbada8b395f61114f0
Aleksei-Zaichenko <aleksei.zaichenko@cix.csi.cuny.edu>
2020-02-18 18:41:08 -0500

    something appers on the screen

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -4,13 +4,17 @@ import CommentInput from "./CommentInput";
 import Comment from "./Comment";
 import "./Comment.css";
 
-const CommentSection = props => {
+const CommentSection = ({comments}) => {
   // Add state for the comments
+
+  const [data] = useState(comments);
 
   return (
     <div>
       {/* map through the comments data and return the Comment component */}
-      <CommentInput />
+      {data.map( comment =>(
+      <CommentInput comment ={comment}/>
+      ))}
     </div>
   );
 };
--- src/components/PostsContainer/Post.js
+++ src/components/PostsContainer/Post.js
@@ -1,5 +1,5 @@
 // You will add code in this file
-import React from "react";
+import React, {useState} from "react";
 import CommentSection from "../CommentSection/CommentSectionContainer";
 import LikeSection from "./LikeSection";
 import PostHeader from "./PostHeader";
@@ -9,6 +9,12 @@ import "./Posts.css";
 // pass props in this file to
 const Post = props => {
   // set up state for the likes
+
+  const [likesCount, setLikesCount] = useState(props.post.likes);
+
+  const incrementLikes = () => {
+    setLikesCount(likesCount => likesCount + 1);
+  };
 
   return (
     <div className="post-border">
@@ -25,7 +31,10 @@ const Post = props => {
           src={props.post.imageUrl}
         />
       </div>
-      <LikeSection />
+      <LikeSection 
+        incrementLikes = {incrementLikes} 
+        likesCount = {likesCount}
+      />
       <CommentSection
         postId={props.post.imageUrl}
         comments={props.post.comments}
